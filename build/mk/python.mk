97fa145bcfe0a62a1d5b68d28b01bc97f2c76b46
Seth Wright <seth@crosse.org>
2017-05-02 11:09:34 -0400

    add 'python' metatarget to install py2 and py3

--- build/mk/python.mk
+++ build/mk/python.mk
@@ -13,4 +13,7 @@ python2: $(PYENV_PATH)/versions/$(PY2_VER)
 python3:	##@languages Install Python 3 (uses pyenv).
 python3: $(PYENV_PATH)/versions/$(PY3_VER)
 
-.PHONY: pyenv python27 python36
+python:		##@languages Install Python 2 and 3 (uses pyenv).
+python: python2 python3
+
+.PHONY: pyenv python27 python36 python
