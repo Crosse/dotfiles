9233d4c9087943d4a055b1f8e9f39d1c67ffdb36
Seth Wright <seth@crosse.org>
2017-09-29 22:31:50 -0400

    make my dotfiles work with OpenBSD again

--- build/dotfiles.sh
+++ build/dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e -o pipefail
+set -e
 
 . ./funcs.sh
 
@@ -9,22 +9,23 @@ cd "$WRKDIR"
 
 for file in $(find "$WRKDIR"            \
                 -mindepth 1 -maxdepth 1	\
-                -not -name '.git'	\
-                -not -name '.gitignore'	\
-                -not -name '.gitmodules'\
-                -not -name 'README.md'	\
-                -not -name 'LICENSE'	\
-                -not -name 'bin'	\
-                -not -name 'build'	\
-                -not -name 'etc'	\
-                -not -name '.*.swp'	\
+                \! -name '.git'	\
+                \! -name '.gitignore'	\
+                \! -name '.gitmodules'\
+                \! -name 'README.md'	\
+                \! -name 'LICENSE'	\
+                \! -name 'bin'	\
+                \! -name 'build'	\
+                \! -name 'etc'	\
+                \! -name '.*.swp'	\
             ); do
     f=$(basename $file)
     if [ -e "${HOME}/$f" -a ! -L "${HOME}/$f" ]; then
         echo "Backing up ${HOME}/$f to ${HOME}/backup/$f"
         [ -d "${HOME}/backup" ] || mkdir -p "${HOME}/backup"
-        mv "${HOME}/$f" "${HOME}/backup/" && ln -sfnv "$file" "${HOME}/$f"
+        mv "${HOME}/$f" "${HOME}/backup/" && ln -sfn "$file" "${HOME}/$f"
     else
-        ln -sfnv "$file" "${HOME}/$f"
+        echo "$file --> ${HOME}/$f"
+        ln -sfn "$file" "${HOME}/$f"
     fi
 done
--- build/symlink.sh
+++ build/symlink.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e -o pipefail
+set -e
 
 BINDIR=$(cd "$(dirname "$0")/../bin" && pwd -P)
 cd "$BINDIR"
@@ -9,13 +9,14 @@ if [ ! -d "${HOME}/bin" ]; then
     mkdir -p "${HOME}/bin"
 fi
 
-for file in $(find "$BINDIR" -maxdepth 1 -mindepth 1 -not -name '.*.swp'); do
+for file in $(find "$BINDIR" -maxdepth 1 -mindepth 1 \! -name '.*.swp'); do
     f=$(basename $file)
     if [ -e "${HOME}/$f" -a ! -L "${HOME}/$f" ]; then
         echo "Backing up ${HOME}/$f to ${HOME}/backup/$f"
         [[ -d "${HOME}/backup" ]] || mkdir -p "${HOME}/backup"
         mv "${HOME}/$f" "${HOME}/backup/" && ln -sfnv "$file" "${HOME}/bin/$f"
     else
-        ln -sfnv "$file" "${HOME}/bin/$f"
+        echo "$file --> ${HOME}/bin/$f"
+        ln -sfn "$file" "${HOME}/bin/$f"
     fi
 done
