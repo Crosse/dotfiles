ee467e8bce652f64cac628c94e6b8e16a929dc72
Seth Wright <seth.wright@fireeye.com>
2019-02-01 16:43:05 -0500

    Add a bunch of stuff that was only present on the work laptop

---
+++ .lein/profiles.clj
@@ -0,0 +1,7 @@
+{:user {
+        :plugins [
+                  [cider/cider-nrepl "0.18.0"]
+                  [jonase/eastwood "0.3.3"] 
+                  [lein-cljfmt "0.6.2"]
+                  [refactor-nrepl "2.4.0"]
+                  ]}}
