d0c4b4271653cea40593aa0da321a2192717f195
Seth Wright <seth.wright@fireeye.com>
2019-07-19 22:31:54 -0400

    clean up anyenv installation and install anyenv-update

--- build/mk/anyenv.mk
+++ build/mk/anyenv.mk
@@ -3,8 +3,10 @@ ANYENV_PATH := ${HOME}/.anyenv
 
 $(ANYENV_PATH):
 ifdef GIT
-	git clone -q https://github.com/riywo/anyenv ${HOME}/.anyenv
-	$(HOME)/.anyenv/bin/anyenv install --force-init
+	git clone -q https://github.com/riywo/anyenv $(ANYENV_PATH)
+	mkdir -p $(ANYENV_PATH)/plugins
+	git clone https://github.com/znz/anyenv-update.git $(ANYENV_PATH)/plugins/anyenv-update
+	$(ANYENV_PATH)/bin/anyenv install --force-init
 else
 	$(warning anyenv requires git to be installed)
 endif
@@ -16,9 +18,9 @@ ifneq ($(wildcard $ANYENV_PATH/bin/anyenv),)
 envs := $(strip $(shell $(ANYENV_PATH)/bin/anyenv install -l | awk '/^ / { print $1 }'))
 $(addprefix $(ANYENV_PATH)/envs/,$(envs)): |$(ANYENV_PATH)
 	eval "$$($(ANYENV_PATH)/bin/anyenv init -)" && \
-	    ${HOME}/.anyenv/bin/anyenv install -s $(@F)
+	    $(ANYENV_PATH)/bin/anyenv install -s $(@F)
 endif
-		
+
 showenvs:
 	@echo "Discovered envs:"
 	@echo $(envs)
