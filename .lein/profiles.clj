db9715dc414313f526c688c9dced6315044c63a7
Seth Wright <seth.wright@fireeye.com>
2019-12-30 11:02:52 -0500

    remove cider-nrepl Lein plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,6 @@
 {:user {
         :plugins [
                   [refactor-nrepl "2.4.0"]
-                  [cider/cider-nrepl "0.21.1"]
                   [jonase/eastwood "0.3.5"]
                   [lein-cljfmt "0.6.4"]
                   ]}}
