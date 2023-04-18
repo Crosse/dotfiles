c5b63cbf26fcfeb7d34c5bd183470033179864bc
Seth Wright <seth@crosse.org>
2023-04-18 15:45:14 -0400

    [lein] Update plugins and add Clojars repo
    (I guess those gpg settings from the previous commit _might_ be useful,
    after all.)

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
-{:user {
-        :plugins [
-                  [refactor-nrepl "2.4.0"]
-                  [jonase/eastwood "0.3.5"]
-                  [lein-cljfmt "0.6.4"]
-                  ]}}
+{:user {:plugins [;; [refactor-nrepl "3.6.0"]
+                  ;; [cider/cider-nrepl "0.28.7"]
+                  ;; [jonase/eastwood "1.3.0"]
+                  ;;[lein-cljfmt "0.9.0"]
+                  ]
+        :repositories [["clojars" {:creds :gpg}]
+                       ["releases" {:url "https://repo.clojars.org" :creds :gpg}]]}}
